3a4459065fdef795751545be0771249b36ac16d1
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-07 22:55:42 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.04 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,21 @@
+import re 
+
+def solution(files):
+    lst = []
+    for i, file in enumerate(files):
+        head = ''
+        number = ''
+        tail = False
+        for ch in file:
+            if '0' <= ch <= '9':
+                number += ch
+                tail = True
+            elif not tail:
+                head += ch
+            else:
+                break
+        lst.append((i, head.lower(), int(number))) 
+        
+    lst.sort(key=lambda x:(x[1], x[2], x[0]))
+    
+    return [files[x[0]] for x in lst]
