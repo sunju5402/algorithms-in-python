5bc4e84762bfe8728a9fa27f80c06b5df5417a1d
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-13 01:18:29 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,6 +1,6 @@
 def solution(data, col, row_begin, row_end):
-    data.sort(reverse=True)
-    data.sort(key=lambda x:x[col - 1])
+    # data.sort(reverse=True)
+    data.sort(key=lambda x:(x[col - 1], -x[0])) # 한번에 오름, 내림차순 가능
     lst = [0] * (row_end - row_begin + 1)
     for i in range(row_begin - 1, row_end):
         for j in range(len(data[i])):
