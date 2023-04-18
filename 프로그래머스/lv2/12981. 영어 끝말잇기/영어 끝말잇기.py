d3cc3728429d9e8f0a2ea47da5fe4fb8d2ff8a22
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-19 04:17:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -4,7 +4,6 @@ def solution(n, words):
     for i in range(len(words)):
         if st:
             if words[i] in st or words[i][0] != words[i - 1][-1]:
-                p = n if (i + 1) % n == 0 else (i + 1) % n
-                return [p, i // n + 1]
+                return [i % n + 1, i // n + 1]
         st.add(words[i])
     return answer
