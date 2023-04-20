656f6dc5fee1df2fdbf34ccd650c0559364e064f
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-20 18:47:36 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,20 +1,8 @@
 import math
 def solution(n,a,b):
     answer = 0
-    while True:
-        if a == b:
-            break
-        
-        if a % 2 == 0:
-            a //= 2
-        else:
-            a += 1
-            a //= 2
-        
-        if b % 2 == 0:
-            b //= 2
-        else:
-            b += 1
-            b //= 2
+    while a != b:
+        a = (a + 1) // 2
+        b = (b + 1) // 2
         answer += 1
     return answer
