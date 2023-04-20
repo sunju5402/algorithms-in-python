00496a321911920d71486adecc4fb4c6f16dda23
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-20 17:39:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 89.14 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,12 @@
+import math
+def solution(n):
+    answer = 1 # 1칸짜리로만 채워졌을 경우
+    cnt_2 = n // 2 # n에 들어갈 수 있는 2의 최대 개수
+    for i in range(1, cnt_2 + 1):
+        cnt_1 = n - i * 2 # 1이 들어갈 수 있는 개수
+        if cnt_1 == 0:
+            answer += 1
+        else:
+            print(cnt_1)
+            answer += math.comb(cnt_1 + i, i)
+    return answer % 1234567
