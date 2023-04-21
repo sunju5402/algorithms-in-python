5a1490f2ff3da16d7d4b1da71396274abae7bfc7
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-22 02:24:13 +0900

    [level 2] Title: 하노이의 탑, Time: 9.97 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,13 +1,4 @@
 def solution(n):
-    # answer = [[0, 0] for _ in range(2 * n - 1)]
-    
-    # for i in range(len(answer)):
-    #     if i + 1 <= n - 1:
-    #         answer[i][0], answer[i][1] = 1, 2
-    #     elif i + 1 == n:
-    #         answer[i][0], answer[i][1] = 1, 3
-    #     else:
-    #         answer[i][0], answer[i][1] = 2, 3
     answer = []
     def hanoi(start, to, mid, n):
         if n == 1:
