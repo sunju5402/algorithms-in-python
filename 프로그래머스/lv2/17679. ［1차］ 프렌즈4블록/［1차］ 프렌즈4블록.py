b30d83653321ff9298246cef2628d197af4d1b9b
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-21 16:00:25 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.22 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -0,0 +1,38 @@
+def solution(m, n, board):
+    for i in range(m):
+        board[i] = list(board[i])
+
+    answer = 0
+    s = set()
+    while True:
+        for i in range(m - 1):
+            for j in range(n - 1):
+                ch = board[i][j]
+                if not ch:
+                    continue
+                if board[i + 1][j] == ch and board[i][j + 1] == ch and board[i + 1][j + 1] == ch:
+                    s.add((i, j))
+                    s.add((i + 1, j))
+                    s.add((i, j + 1))
+                    s.add((i + 1, j + 1))
+        
+        if s:
+            answer += len(s)
+            for i, j in s:
+                board[i][j] = []
+            s = set()
+        else: # 아예 겹치는 게 없을 때 반환한다.
+            return answer
+        
+        while True:
+            move = False
+            for i in range(m - 1):
+                for j in range(n):
+                    if board[i][j] and not board[i + 1][j]:
+                        board[i + 1][j] = board[i][j]
+                        board[i][j] = []
+                        move = True
+            if not move:
+                break
+                
+    return answer
