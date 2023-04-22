a5490bdc694e0edeee2bdbf104250e343daaf1fe
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-23 01:38:03 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -0,0 +1,20 @@
+from collections import deque
+def solution(priorities, location):
+    answer = 0
+    q = deque()
+    
+    for i, p in enumerate(priorities):
+        q.append([p, chr(ord("A") + i)])   
+    
+    while q:
+        m = max(q, key=lambda x : x[0])
+        while q:
+            val = q.popleft()
+            if m[0] == val[0]:
+                answer += 1
+                if m[1] == chr(ord("A") + location):
+                    return answer
+                break
+            else:
+                q.append(val) 
+    return answer
