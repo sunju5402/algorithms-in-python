ed7de1c6e44d9d2e5f435f446e4d471e556ab475
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-26 12:42:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.77 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def solution(maps):
+    dx = [0, 0, 1, -1] # 동서남북
+    dy = [1, -1, 0, 0]
+    
+    q = deque()
+    q.append((0, 0))
+    
+    n = len(maps)
+    m = len(maps[0])
+    
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 벽을 만나는 경우
+            if maps[nx][ny] == 0:
+                continue
+            if maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                q.append((nx, ny))
+                
+    return -1 if maps[n - 1][m - 1] == 1 else maps[n - 1][m - 1]
