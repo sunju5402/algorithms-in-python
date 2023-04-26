0d7bcac9a5c2041aa45c026602c65ac11cde5651
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-26 13:00:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -9,9 +9,11 @@ def solution(maps):
     
     n = len(maps)
     m = len(maps[0])
-    
+    visited = [[0 for i in range(m)] for i in range(n)]
+
     while q:
         x, y = q.popleft()
+        visited[x][y] = 1
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -20,8 +22,8 @@ def solution(maps):
             # 벽을 만나는 경우
             if maps[nx][ny] == 0:
                 continue
-            if maps[nx][ny] == 1:
+            if not visited[nx][ny] and maps[nx][ny] == 1:
                 maps[nx][ny] = maps[x][y] + 1
                 q.append((nx, ny))
-                
+
     return -1 if maps[n - 1][m - 1] == 1 else maps[n - 1][m - 1]
