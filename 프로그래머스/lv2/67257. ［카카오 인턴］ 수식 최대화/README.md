c30af2df718653ea560a38038189855c9e010783
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-26 22:51:05 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/README.md
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/README.md
@@ -1,10 +1,10 @@
 # [level 2] [카카오 인턴] 수식 최대화 - 67257 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67257) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67257?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 0.07 ms
+메모리: 10.2 MB, 시간: 0.32 ms
 
 ### 구분
 
--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,37 +1,49 @@
-from itertools import permutations
-from collections import deque
+# from itertools import permutations
+# from collections import deque
 
+# def solution(expression):
+#     answer = 0
+#     op = set()
+#     stack = []
+#     n = ""
+#     for ch in expression:
+#         if ch.isdigit():
+#             n += ch
+#         else:
+#             stack.append(int(n))
+#             stack.append(ch)
+#             n = ""
+#             op.add(ch)
+#     stack.append(int(n))
+    
+    
+#     for p in list(permutations(op, len(op))):
+#         lst = deque(stack)
+#         for i in p:
+#             lst2 = deque()
+#             while lst:
+#                 o = lst.popleft()
+#                 if o == i:
+#                     if i == "*":
+#                         lst2.append(lst2.pop() * lst.popleft())
+#                     elif i == "+":
+#                         lst2.append(lst2.pop() + lst.popleft())
+#                     else:
+#                         lst2.append(lst2.pop() - lst.popleft())
+#                 else:
+#                     lst2.append(o)
+#             lst = lst2   
+#         answer = max(answer, abs(lst[-1]))
+#     return answer
 def solution(expression):
-    answer = 0
-    op = set()
-    stack = []
-    n = ""
-    for ch in expression:
-        if ch.isdigit():
-            n += ch
-        else:
-            stack.append(int(n))
-            stack.append(ch)
-            n = ""
-            op.add(ch)
-    stack.append(int(n))
-    
-    
-    for p in list(permutations(op, len(op))):
-        lst = deque(stack)
-        for i in p:
-            lst2 = deque()
-            while lst:
-                o = lst.popleft()
-                if o == i:
-                    if i == "*":
-                        lst2.append(lst2.pop() * lst.popleft())
-                    elif i == "+":
-                        lst2.append(lst2.pop() + lst.popleft())
-                    else:
-                        lst2.append(lst2.pop() - lst.popleft())
-                else:
-                    lst2.append(o)
-            lst = lst2   
-        answer = max(answer, abs(lst[-1]))
-    return answer
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
