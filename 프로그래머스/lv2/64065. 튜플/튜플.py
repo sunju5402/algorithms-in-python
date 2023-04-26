df36b364c1d4228476aa318548435924c47dae84
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-26 15:56:58 +0900

    [level 2] Title: 튜플, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -3,15 +3,11 @@ def solution(s):
     lst = list(map(str, s.split("},")))
     
     for i in range(len(lst)):
-        lst[i] = lst[i].replace("{", "")
-        lst[i] = lst[i].replace("}", "")
-        lst[i] = lst[i].split(",")
+        lst[i] = lst[i].replace("{", "").replace("}", "").split(",")
     
     lst.sort(key=lambda x : len(x))
     
     for a in lst:
-        for b in answer:
-            a.remove(b)
-        answer.append(a[0])
+        answer.append(list((set(a) - set(answer)))[0])
             
     return [int(a) for a in answer]
