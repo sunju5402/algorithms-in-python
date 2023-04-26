39caa2687f1ae822fc0286942350c8158eeb1bdd
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-27 07:51:03 +0900

    [level 2] Title: 방문 길이, Time: 0.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/README.md
+++ 프로그래머스/lv2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.52 ms
+메모리: 10.3 MB, 시간: 0.25 ms
 
 ### 구분
 
--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -1,29 +1,13 @@
 from collections import deque
 
 def solution(dirs):
-    answer = 0
-    q = deque()
+    s = set()
+    d = {'U': (0,1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
     x, y = 0, 0
-    nx, ny = 0, 0
-
-    for d in dirs:
-        if d == "U":
-            ny = y + 1 
-        elif d == "D":
-            ny = y - 1 
-        elif d == "R":
-            nx = x + 1
-        else:
-            nx = x - 1
-            
-        if nx < -5 or nx > 5 or ny < -5 or ny > 5:
-            nx, ny = x, y
-            continue
-        
-        if (x, y, nx, ny) not in q and (nx, ny, x, y) not in q:
-            q.append((x, y, nx, ny))
-            answer += 1
-            
-        x, y = nx, ny
-    
-    return answer
+    for i in dirs:
+        nx, ny = x + d[i][0], y + d[i][1]
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            s.add((x,y,nx,ny))
+            s.add((nx,ny,x,y))
+            x, y = nx, ny
+    return len(s)//2
