77e1e56284031fdf2006aafb8a9ab30d758c1eba
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-27 16:43:01 +0900

    [level 2] Title: 후보키, Time: 1.87 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -1,24 +1,22 @@
 from itertools import combinations
 
 def solution(relation):
-    col = len(relation[0])
-    lst = [i for i in range(col)]
-    com = [c for i in range(1, len(lst) + 1) for c in list(combinations(lst, i))] # 속성에 대한 조합
-    
-    ck_com = set() # 후보키가 될 수 있는 조합
-    for c in com:
-        s = set()
-        for ck in ck_com:
-            if set(ck).issubset(set(c)): # 최소성 검증
-                break
-        else :
-            for r in relation:
-                l = []
-                for i in c:
-                    l.append(r[i])
-                s.add(tuple(l))
-        
-            if len(s) == len(relation): # 유일성 검증
-                ck_com.add(c)
-                
-    return len(ck_com)
+    answer_list = list()
+    for i in range(1, 1 << len(relation[0])):
+        tmp_set = set()
+        for j in range(len(relation)):
+            tmp = ''
+            for k in range(len(relation[0])):
+                if i & (1 << k):
+                    tmp += str(relation[j][k])
+            tmp_set.add(tmp)
+
+        if len(tmp_set) == len(relation):
+            not_duplicate = True
+            for num in answer_list:
+                if (num & i) == num:
+                    not_duplicate = False
+                    break
+            if not_duplicate:
+                answer_list.append(i)
+    return len(answer_list)
