393cd4b7f1d995163333248071b06cf33b0b70f9
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-27 09:03:22 +0900

    [level 2] Title: 스킬트리, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/README.md
+++ 프로그래머스/lv2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10.3 MB, 시간: 0.00 ms
 
 ### 구분
 
--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -2,23 +2,23 @@ def solution(skill, skill_trees):
     answer = 0
     dic = {s : -1 for s in skill}
     
-    if len(skill) == 1:
+    if len(skill) == 1: # 스킬 순서가 하나밖에 없으면 무조건 다 가능한 스킬트리이다.
         return len(skill_trees)
-    
     
     for st in skill_trees:
         dic2 = dict(dic)
-        available = True
+        available = True # 가능한 스킬트리인지 여부
+        
         for i, ch in enumerate(skill):
             idx = st.find(ch)
-            if idx != -1:
-                if i != 0:
-                    if dic2[skill[i - 1]] == -1 or dic2[skill[i - 1]] > idx:
+            if idx != -1: # 스킬트리에 스킬이 있을 경우
+                if i != 0: 
+                    # 현재의 스킬이 이전 스킬보다 앞에 나오거나 이전 스킬이 존재하지 않는데 현재 스킬이 존재하는 경우
+                    if dic2[skill[i - 1]] == -1 or dic2[skill[i - 1]] > idx: 
                         available = False
                         break
                 dic2[ch] = idx
                 
-        print(dic2)
         if available:
             answer += 1
     
