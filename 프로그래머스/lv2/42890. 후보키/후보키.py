7929c21766e091f1b56c08b40db18d3647e693b4
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-27 16:45:22 +0900

    [level 2] Title: 후보키, Time: 0.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -3,22 +3,22 @@ from itertools import combinations
 def solution(relation):
     col = len(relation[0])
     lst = [i for i in range(col)]
-    com = [c for i in range(1, len(lst) + 1) for c in list(combinations(lst, i))]
-
-    com_set = list()
+    com = [c for i in range(1, len(lst) + 1) for c in list(combinations(lst, i))] # 속성에 대한 조합
+    
+    ck_com = set() # 후보키가 될 수 있는 조합
     for c in com:
         s = set()
-        for r in relation:
-            l = []
-            for i in c:
-                l.append(r[i])
-            s.add(tuple(l))
-
-        if len(s) == len(relation):
-            for cs in com_set:
-                if set(cs).issubset(set(c)):
-                    break
-            else:
-                com_set.append(c)
-
-    return len(com_set)
+        for ck in ck_com:
+            if set(ck).issubset(set(c)): # 최소성 검증
+                break
+        else :
+            for r in relation:
+                l = []
+                for i in c:
+                    l.append(r[i])
+                s.add(tuple(l))
+        
+            if len(s) == len(relation): # 유일성 검증
+                ck_com.add(c)
+                
+    return len(ck_com)
