6f380419ed7d79283ebdaeaeadb950be01d88b0b
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-27 09:50:15 +0900

    [level 2] Title: 타겟 넘버, Time: 1.50 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 11.1 MB, 시간: 13.41 ms
+메모리: 10.2 MB, 시간: 1.50 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,6 @@
 from itertools import product
 
 def solution(numbers, target):
-    answer = 0
-    
-    for p in list(product(['+', '-'], repeat=len(numbers))):
-        total = 0
-        for n, op in zip(numbers, p):
-            if op == '+':
-                total += n
-            else:
-                total -= n
-            
-        if total == target:
-            answer += 1
-    
-    return answer
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
