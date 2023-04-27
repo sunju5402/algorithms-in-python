d1e69e15edfddd33acd8aa383504da46b4bc2d16
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-27 16:43:38 +0900

    [level 2] Title: 후보키, Time: 0.64 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -1,22 +1,24 @@
 from itertools import combinations
 
 def solution(relation):
-    answer_list = list()
-    for i in range(1, 1 << len(relation[0])):
-        tmp_set = set()
-        for j in range(len(relation)):
-            tmp = ''
-            for k in range(len(relation[0])):
-                if i & (1 << k):
-                    tmp += str(relation[j][k])
-            tmp_set.add(tmp)
+    col = len(relation[0])
+    lst = [i for i in range(col)]
+    com = [c for i in range(1, len(lst) + 1) for c in list(combinations(lst, i))]
 
-        if len(tmp_set) == len(relation):
-            not_duplicate = True
-            for num in answer_list:
-                if (num & i) == num:
-                    not_duplicate = False
+    com_set = list()
+    for c in com:
+        s = set()
+        for r in relation:
+            l = []
+            for i in c:
+                l.append(r[i])
+            s.add(tuple(l))
+
+        if len(s) == len(relation):
+            for cs in com_set:
+                if set(cs).issubset(set(c)):
                     break
-            if not_duplicate:
-                answer_list.append(i)
-    return len(answer_list)
+            else:
+                com_set.append(c)
+
+    return len(com_set)
