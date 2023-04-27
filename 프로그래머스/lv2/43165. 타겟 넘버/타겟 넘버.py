7d20b17e4494fb068c3998ebe292374948efd9a4
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-04-27 09:50:35 +0900

    [level 2] Title: 타겟 넘버, Time: 3.93 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,6 +1,9 @@
 from itertools import product
 
 def solution(numbers, target):
-    l = [(x, -x) for x in numbers]
-    s = list(map(sum, product(*l)))
-    return s.count(target)
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
