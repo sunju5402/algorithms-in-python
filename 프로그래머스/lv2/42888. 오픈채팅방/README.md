9d4193e26a53d87561d8beeb05319cec10082b07
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-11 18:34:30 +0900

    [level 2] Title: 오픈채팅방, Time: 92.10 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/README.md
+++ 프로그래머스/lv2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=python3) 
 
 ### 성능 요약
 
-메모리: 38.3 MB, 시간: 68.82 ms
+메모리: 38.3 MB, 시간: 92.10 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,20 +1,14 @@
 def solution(record):
-    dic = {}
-    for r in record:
-        s = r.split()
-        if len(s) == 3:
-            dic[s[1]] = s[2]
-            
     answer = []
+    namespace = {}
+    printer = {'Enter':'님이 들어왔습니다.', 'Leave':'님이 나갔습니다.'}
     for r in record:
-        s = r.split()
-        word = dic[s[1]]
-        if s[0] == "Enter":
-            word += "님이 들어왔습니다."
-        elif s[0] == "Leave":
-            word += "님이 나갔습니다."
-        else:
-            continue
-        answer.append(word)
-        
+        rr = r.split(' ')
+        if rr[0] in ['Enter', 'Change']:
+            namespace[rr[1]] = rr[2]
+
+    for r in record:
+        if r.split(' ')[0] != 'Change':
+            answer.append(namespace[r.split(' ')[1]] + printer[r.split(' ')[0]])
+
     return answer
