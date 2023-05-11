eb7907bd0453931a588385517e777ba021847950
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-12 00:58:52 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,18 @@
+def solution(name):
+    answer = 0
+
+    if len(name) == name.count('A'): # 모두 A일 경우는 이동 필요 없음
+        return 0
+    
+    l = len(name) - 1 # 최대 이동한 경우
+    for i, s in enumerate(name):
+        answer += min(ord(s) - ord('A'), ord('Z') - ord(s) + 1) # +1은 A -> Z로 움직이는 경우
+        
+        nxt = i + 1
+        while nxt < len(name) and name[nxt] == 'A':
+            nxt += 1
+        
+        l =  min([l, 2 * i + len(name) - nxt, i + 2 * (len(name) - nxt)]) # 기존, 왼쪽, 오른쪽 시작
+    
+    return answer + l
+        
