e2b9c2833956ced0be36c46dfa7e7f3cf0d93834
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-11 18:34:54 +0900

    [level 2] Title: 오픈채팅방, Time: 68.82 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,14 +1,20 @@
 def solution(record):
+    dic = {}
+    for r in record:
+        s = r.split()
+        if len(s) == 3:
+            dic[s[1]] = s[2]
+            
     answer = []
-    namespace = {}
-    printer = {'Enter':'님이 들어왔습니다.', 'Leave':'님이 나갔습니다.'}
     for r in record:
-        rr = r.split(' ')
-        if rr[0] in ['Enter', 'Change']:
-            namespace[rr[1]] = rr[2]
-
-    for r in record:
-        if r.split(' ')[0] != 'Change':
-            answer.append(namespace[r.split(' ')[1]] + printer[r.split(' ')[0]])
-
+        s = r.split()
+        word = dic[s[1]]
+        if s[0] == "Enter":
+            word += "님이 들어왔습니다."
+        elif s[0] == "Leave":
+            word += "님이 나갔습니다."
+        else:
+            continue
+        answer.append(word)
+        
     return answer
