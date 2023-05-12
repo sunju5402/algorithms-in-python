6581bd237814efb4704dafa9000378e4db4e60fd
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-12 20:10:10 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -4,12 +4,8 @@ def solution(n):
     tmp = "124"
     
     while n > 0:
-        if n % 3 == 0:
-            answer = tmp[2] + answer
-            n //= 3
-            n -= 1
-        else:
-            answer = tmp[n % 3 - 1] + answer
-            n //= 3
+        n -= 1
+        answer = tmp[n % 3] + answer
+        n //= 3
     
     return answer
