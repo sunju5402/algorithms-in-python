348ef9de50305b808e23c40d8a3061c8c8c35b6f
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-12 23:43:23 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,19 +1,24 @@
 def solution(s):
     answer = 0
+    
+    if len(s) % 2 != 0: # 홀수개의 괄호는 무조건 짝이 안 맞는다.
+        return 0
+    
     for i in range(len(s)):
         stack = []
         s = s[1:] + s[:1] if i != 0 else s
         
         for j in range(len(s)):
-            if stack:
-                if s[j] == '(' or s[j] == '{' or s[j] == '[':
-                    stack.append(s[j])
-                else:
-                    if (s[j] == ')' and stack[-1] == '(') or (s[j] == '}' and stack[-1] == '{') or (s[j] == ']' and stack[-1] == '['):
-                        stack.pop()
-            else:
+            if s[j] == '(' or s[j] == '{' or s[j] == '[':
                 stack.append(s[j])
-        if not stack:
+            else:
+                if not stack:
+                    break
+                if (s[j] == ')' and stack[-1] == '(') or (s[j] == '}' and stack[-1] == '{') or (s[j] == ']' and stack[-1] == '['):
+                    stack.pop()
+                else:
+                    break
+        else:
             answer += 1
     
     return answer
