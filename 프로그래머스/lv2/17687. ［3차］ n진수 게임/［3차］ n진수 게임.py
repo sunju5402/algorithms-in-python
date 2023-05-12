86190c949b5de7c1852b04dc0ed98d72782cd371
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-12 19:34:10 +0900

    [level 2] Title: [3차] n진수 게임, Time: 6.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -18,23 +18,21 @@
 #         p += m
     
 #     return answer
-big = ["A","B","C","D","E","F"]
 def solution(n, t, m, p):
-    a="0"
-    i=0
-
+    tmp = "0123456789ABCDEF"
+    a = "01"
+    i = 2
     while True:
-        if len(a)>=t*m:
+        if len(a) >= t * m:
             break
-        b=""
-        j=i
-        while (j):
-            if j%n>9:
-                b=big[j%n-10]+b
-            else:
-                b=str(j%n)+b
-            j=j//n
-        a=a+b
-        i=i+1
-    answer = a[p-1::m][:t]
+            
+        b = ""
+        j = i
+        while j:
+            b = str(tmp[j % n]) + b
+            j //= n
+        a += b
+        i += 1
+    answer = a[p - 1::m][:t]
+    
     return answer
