a637f8c51e4b88a6596d44758743eeb76f03aa64
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-12 19:16:29 +0900

    [level 2] Title: [3차] n진수 게임, Time: 4.35 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/README.md
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] n진수 게임 - 17687 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 13.41 ms
+메모리: 10.1 MB, 시간: 4.35 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,20 +1,40 @@
-def convert(num, base):
-    tmp = "0123456789ABCDEF"
-    a, b = divmod(num, base)
+# def convert(num, base):
+#     tmp = "0123456789ABCDEF"
+#     a, b = divmod(num, base)
     
-    if a == 0:
-        return tmp[b]
-    return convert(a, base) + tmp[b]
+#     if a == 0:
+#         return tmp[b]
+#     return convert(a, base) + tmp[b]
 
+# def solution(n, t, m, p):
+#     answer = ''
+#     s = '01' # 2진법부터 시작이므로 '01'로 초기화
+    
+#     for i in range(2, m * t):
+#         s += str(convert(i, n))
+    
+#     while len(answer) < t:
+#         answer += s[p - 1] # index이므로 -1
+#         p += m
+    
+#     return answer
+big = ["A","B","C","D","E","F"]
 def solution(n, t, m, p):
-    answer = ''
-    s = '01' # 2진법부터 시작이므로 '01'로 초기화
-    
-    for i in range(2, m * t):
-        s += str(convert(i, n))
-    
-    while len(answer) < t:
-        answer += s[p - 1] # index이므로 -1
-        p += m
-    
+    a="0"
+    i=0
+
+    while True:
+        if len(a)>=t*m:
+            break
+        b=""
+        j=i
+        while (j):
+            if j%n>9:
+                b=big[j%n-10]+b
+            else:
+                b=str(j%n)+b
+            j=j//n
+        a=a+b
+        i=i+1
+    answer = a[p-1::m][:t]
     return answer
