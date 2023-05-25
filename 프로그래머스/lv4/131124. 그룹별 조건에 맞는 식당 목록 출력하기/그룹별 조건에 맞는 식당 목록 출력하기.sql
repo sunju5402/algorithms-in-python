b68f9603b6c29b525dc609af69210df0e1f669d0
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-26 03:36:37 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,12 +1,26 @@
 -- 코드를 입력하세요
-select a.member_name, b.review_text, date_format(b.review_date, '%Y-%m-%d') as review_date
-from member_profile a join rest_review b on a.member_id = b.member_id
-where a.member_id in (SELECT a.member_id
-from member_profile a join rest_review b on a.member_id = b.member_id
-group by a.member_id
-having count(*) = (select count(*)
-                  from rest_review
-                  group by member_id
-                  order by count(*) desc
-                  limit 1)) 
-order by review_date, b.review_text
+select b.member_name, a.review_text, date_format(a.review_date, '%Y-%m-%d') as review_date
+from rest_review a join (
+    select a.member_id, a.member_name
+    from member_profile a join rest_review b on a.member_id = b.member_id
+    group by a.member_id
+    having count(*) = (
+        select count(*)
+        from rest_review
+        group by member_id
+        order by count(*) desc
+        limit 1)) b on a.member_id = b.member_id
+order by a.review_date, a.review_text;
+
+# SELECT B.MEMBER_NAME, A.REVIEW_TEXT, DATE_FORMAT(A.REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
+# FROM REST_REVIEW A
+# JOIN (
+#     SELECT R.MEMBER_ID, M.MEMBER_NAME, RANK() OVER(ORDER BY CNT DESC) AS RANKING
+#     FROM (
+#         SELECT *, COUNT(MEMBER_ID) AS CNT
+#         FROM REST_REVIEW
+#         GROUP BY MEMBER_ID) AS R
+#     JOIN MEMBER_PROFILE M ON R.MEMBER_ID = M.MEMBER_ID) B
+# ON A.MEMBER_ID = B.MEMBER_ID
+# WHERE B.RANKING = 1
+# ORDER BY A.REVIEW_DATE, A.REVIEW_TEXT;
