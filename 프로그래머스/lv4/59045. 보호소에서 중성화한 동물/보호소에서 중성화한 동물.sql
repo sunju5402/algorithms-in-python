fbd3c035a97c83b59b3762c58cf8398469e577c9
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-05-26 23:01:40 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,7 +1,19 @@
 -- 코드를 입력하세요
 SELECT c.animal_id, c.animal_type, c.name
 from (select a.animal_id, a.animal_type, a.name, b.sex_upon_outcome
-                     from animal_ins a join animal_outs b on a.animal_id = b.animal_id
-                     where a.sex_upon_intake like 'Intact%') as c
+      from animal_ins a join animal_outs b on a.animal_id = b.animal_id
+        where a.sex_upon_intake like 'Intact%') as c
 where c.sex_upon_outcome like 'Spayed%' or c.sex_upon_outcome like 'Neutered%'
-order by c.animal_id
+order by c.animal_id;
+
+SELECT a.animal_id, a.animal_type, a.name
+from animal_ins a join animal_outs b on a.animal_id = b.animal_id
+where a.sex_upon_intake like 'Intact%' and (b.sex_upon_outcome like 'Spayed%' or b.sex_upon_outcome like 'Neutered%')
+order by a.animal_id;
+
+SELECT a.animal_id, a.animal_type, a.name
+from animal_ins a join animal_outs b on a.animal_id = b.animal_id
+where a.sex_upon_intake like 'Intact%' and b.sex_upon_outcome not in ('Intact Male', 'Intact Female')
+order by a.animal_id;
+
+
